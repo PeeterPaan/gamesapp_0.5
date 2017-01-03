57fe3faa23cef1f9173b4c717c42c840fc222fa4
PeeterPaan <281629@uwr.edu.pl>
2017-01-04 00:08:38 +0100

    Add files via upload

---
+++ gameapp.py
@@ -0,0 +1,158 @@
+from flask import Flask, request, g, render_template, redirect, url_for, session, flash
+import sqlite3 as sql
+from wtforms import Form, TextField, PasswordField, validators, BooleanField
+from passlib.hash import sha256_crypt
+import flask_wtf
+import gc
+app = Flask(__name__)
+app.secret_key = 'sikret ki'
+
+DATABASE = '/Users/PeterPan/Documents/gameapp/userslist.db'
+
+def database_command(command, *args):
+    con = sql.connect('/Users/PeterPan/Documents/gameapp/userslist.db')
+    cur = con.cursor()
+    cur.execute(command, args)
+    rows = cur.fetchall()
+    return rows
+class user(object):
+    """Makes cute animals."""
+    # For initializing our instance objects
+    def __init__(self, username, usertype):
+        self.username = username
+        self.type = usertype
+
+
+
+
+
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min = 4, max = 20)])
+    email = TextField('Email Address', [validators.Length(min = 6, max = 50)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message = 'Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the Terms of Service and Privacy Notice (updated Jan 22, 2015)',
+                              [validators.Required()])
+
+@app.route('/')
+
+def home():
+
+    return redirect('welcome')
+
+@app.route('/welcome')
+def tuna():
+    if session.get('logged_in'):
+        login = session['username']
+        user.username = session['username']
+        database_command("select user_type from users WHERE username = ?", user.username)
+    else:
+        return render_template("welcome.html")
+
+    return render_template("welcome.html", login = login)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        con = sql.connect('/Users/PeterPan/Documents/gameapp/userslist.db')
+        cur = con.cursor()
+
+        cur.execute("SELECT password FROM users WHERE username= ?", (request.form['username'],))
+        check_if_exists = cur.fetchall()
+
+        if check_if_exists:
+
+            if sha256_crypt.verify(request.form['password'], check_if_exists[0][0]):
+                flash('You succesfully logged in, good job nygga')
+                session['logged_in'] = True
+                session['username'] = request.form['username']
+                return redirect(url_for('table'))
+
+            else:
+                flash('Noooope')
+                return render_template('login.html')
+        else:
+            flash('User doesnt exist')
+            return render_template('login.html')
+
+    return render_template('login.html', error=error, login=session['username'])
+
+
+@app.route('/table')
+def table():
+
+    if session.get('logged_in'):
+        rows = database_command("select * from games")
+
+        return render_template("table.html", rows = rows, login = session['username'])
+    else:
+        flash('You are not logged in. Please log in to view rank')
+        return redirect('welcome')
+    print get_flashed_messages()
+
+@app.route('/logout')
+def logout():
+    if session.get('logged_in'):
+        session['logged_in'] = False
+        flash('You succesfully logged out')
+
+        return redirect('welcome')
+
+    else:
+        return redirect('welcome')
+
+
+
+@app.route('/register', methods = ["GET", "POST"])
+
+def register():
+        form = RegistrationForm(request.form)
+
+        if request.method == "POST" and form.validate:
+            username = form.username.data
+            email = form.email.data
+            password = sha256_crypt.encrypt((str(form.password.data)))
+
+            con = sql.connect('/Users/PeterPan/Documents/gameapp/userslist.db')
+            cur = con.cursor()
+            cur.execute("select * from users where Username = ?", (username,))
+            check_if_exists = cur.fetchall()
+
+            if int(len(check_if_exists)) > 0:
+                flash('User already exists')
+                return render_template("register.html", form = form)
+
+            else:
+
+                cur.execute("insert into users (username, password, email) Values(?,?,?)", (username,password,email,))
+                con.commit()
+                flash('Succesfully reigstered, cocksucker.')
+                con.close()
+                gc.collect()
+
+                session['logged_in'] = True
+                session['username'] = username
+                return redirect('/welcome')
+
+        return render_template("register.html", form = form)
+
+@app.route('/whatulike', methods = ["GET", "POST"])
+
+def whatulike():
+
+    if session.get('logged_in'):
+        rows = database_command("select * from games")
+        return render_template("games_search.html", rows = rows, login=session['username'])
+    else:
+        flash('You are not logged in. Please log in to view this page')
+        return redirect('welcome')
+
+#@app.route('/create', methods = ["GET","POST"])
+
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug = True)
